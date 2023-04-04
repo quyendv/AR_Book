45481bacf77aa51895d7838ab2875c474d351dae
quyendv <daovanquyen45@gmail.com>
2023-04-05 01:18:23 +0700

    Fix Error Vite build
    Add '/' before 'src, resourses, ...' when using '@import', 'url()'

--- client/src/components/common/ProductItem.jsx
+++ client/src/components/common/ProductItem.jsx
@@ -2,7 +2,7 @@ function ProductItem() {
   return (
     <div className="w-full group cursor-pointer hover:shadow-productItem hover:rounded-lg transition">
       {/* Image product */}
-      <div className="h-0 bg-[url('src/assets/images/product-item1.png')] bg-cover bg-center bg-no-repeat pb-[100%]"></div>
+      <div className="h-0 bg-[url('/src/assets/images/product-item1.png')] bg-cover bg-center bg-no-repeat pb-[100%]"></div>
       {/* Desc */}
       <p className="text-center text-base text-white group-hover:text-primary">Product 1</p>
     </div>
